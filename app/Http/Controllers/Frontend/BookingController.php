944bed5ee29be2f6def118bea763389d06e9e15f
HumaCode <HumaCode>
2023-10-01 21:35:43 +0700

    membuat halaman booking request (assign room)  part 12 - Backend

--- app/Http/Controllers/Frontend/BookingController.php
+++ app/Http/Controllers/Frontend/BookingController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
+use App\Models\BookingRoomList;
 use App\Models\Room;
 use App\Models\RoomBookedDate;
+use App\Models\RoomNumber;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
@@ -246,7 +248,19 @@ class BookingController extends Controller
 
 
 
-    public function assignRoom()
+    public function assignRoom($booking_id)
     {
+        $booking = Booking::find($booking_id);
+
+        $booking_date_array = RoomBookedDate::where('booking_id', $booking_id)->pluck('booking_date')->toArray();
+        $check_date_booking_ids = RoomBookedDate::whereIn('book_date', $booking_date_array)->where('room_id', $booking->rooms_id)->distinct()->pluck('booking_id')->toArray();
+
+        $booking_ids = Booking::whereIn('id', $check_date_booking_ids)->pluck('id')->toArray();
+
+        $assign_room_ids = BookingRoomList::whereIn('booking_id', $booking_ids)->pluck('room_number_id')->toArray();
+
+        $room_numbers = RoomNumber::where('rooms_id', $booking->rooms_id)->whereNotIn('id', $assign_room_ids)->where('status', 'Active')->get();
+
+        return view('backend.booking.assign_room', compact('booking', 'room_numbers'));
     }
 }
